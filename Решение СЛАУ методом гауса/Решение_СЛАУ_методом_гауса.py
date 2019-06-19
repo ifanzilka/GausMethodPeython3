b68d59c9d89ac2328f87353b2bb388335db8c06d
Фанзилька Набияров <nabiyarov.fanzil@gmail.com>
2019-06-19 15:57:43 +0300

    Улушенная версия

--- Решение СЛАУ методом гауса/Решение_СЛАУ_методом_гауса.py
+++ Решение СЛАУ методом гауса/Решение_СЛАУ_методом_гауса.py
@@ -1,23 +1,53 @@
-def GausMethod(a):#приведение к ступенчатому виду
+#GausMethod
+
+
+def GausMethod(a):                                  #приведение к ступенчатому виду
 
     n=len(a)
     m=len(a[0])
+
     for i in range(n):
 
         for j in range(i+1,n):
             
-            if a[j][i]!=0:
-                kf=float(a[i][i]/a[j][i])
-                for c in range(m):
-                    a[j][c]=a[j][c]*kf
-                    a[j][c]=a[i][c]-a[j][c]
-            print(a)
+            if i<m:
+
+               if a[j][i]!=0 :
+                    if a[i][i]!=0:
+
+                        kf=float(a[i][i]/a[j][i])
+                        for c in range(m):
+                            a[j][c]=a[j][c]*kf
+                            a[j][c]=a[i][c]-a[j][c]
+                    else:                               #если в строке нунужный 0
+                        other=0
+                        stop=0
+                        b1=[0 for j in range(m)]        #строка которую меняем
+                        for z in range(m):
+                            b1[z]=a[i][z]
+
+                        a1=[0 for j in range(m)]        #строка на которю меняем
+                        for k in range(i,n):
+                            if a[k][i]!=0 and stop==0:              #Нашли эту строку
+                               stop=1
+                               other=k
+                               for c in range(m):
+                                    a1[c]=a[k][c]
+                    
+                        for d in range(m):              #меняем строки
+                            a[i][d]=a1[d]
+                        for z in range(m):
+                            a[other][z]=b1[z]
+
+
 def BackwardPass(a,dictonary):#обратный проход
    
      for i in range(1,m):
 
          if i==1:
-             dictonary[m-i]=round(a[m-1-i][m-i]/a[m-i-1][m-i-1])
+             if abs(a[m-i-1][m-i-1])< 0.0000001:
+                 return 0
+             dictonary[m-i]=round(a[m-1-i][m-i]/a[m-i-1][m-i-1],13)
          else:
             sum=0
             list_my=[k for k in range(1,m)]
@@ -29,8 +59,31 @@ def BackwardPass(a,dictonary):#обратный проход
                if j+1 in dictonary:
                     sum=sum+a[m-i-1][j]*dictonary[j+1]
 
-            dictonary[m-i]=round(((a[m-1-i][m-1]-sum))/(a[m-i-1][m-i-1]))
+            dictonary[m-i]=round(((a[m-1-i][m-1]-sum))/(a[m-i-1][m-i-1]),13)
 
+
+def CheckZeroStrok(a,Libary):#проверка на нулевую строку
+
+    for i in range(n-1,1,-1):
+        zero=1
+        for j in range(m):
+            if abs(a[i][j])>1e-10:
+                zero=0
+        if zero==1:
+            Libary[3]=Libary[3]+1
+
+
+
+
+def CheckINF(a,Libary): #проверка на бесконечное кол во решений
+    for i in range(n):
+        if abs(a[i][m-1])<0.0000001 and abs(a[i][m-2])<0.0000001:
+            Libary[0]=1
+
+
+def CheckNo(a,Libarry):# проверка на "Нет решений"
+    if abs(a[n-1][m-2])<1e-10 and  abs(a[n-1][m-1])>1e-10:
+            Libary[1]=1
 
 
      
@@ -40,9 +93,13 @@ s=str(input())
 s=s.split()
 n=int(s[0])
 m=int(s[1])
+m1=m
 m=m+1
 a=[[0 for j in range(m)] for i in range(n)]
 dictonary={}
+Libary=[0 for j in range(4)] # здесь храним значения ["INF","NO","YES,"кол во нулевых строк"] 1 - значит условие верно
+k=1
+
 
 for i in range (n):#считываем данные
     inp=str(input())
@@ -50,9 +107,32 @@ for i in range (n):#считываем данные
     for j in range(m):
         a[i][j]=float(a[i][j])
 
-GausMethod(a)
-BackwardPass(a,dictonary)
 
+GausMethod(a)
+CheckZeroStrok(a,Libary)
+n=n-Libary[3]
+CheckINF(a,Libary)
+CheckNo(a,Libary)
+
+if (n<m1 or Libary[0]==1) and Libary[1]==0:
+    inf=1
+    print("INF")  
+else :
+    if Libary[1]!=1:
+
+        k=BackwardPass(a,dictonary)
+        if k==0:
+             print("NO")
+
+        
+    if Libary[1]==1:
+        print("NO")
+    else :
+        print("YES")
+        k=1
+        for i in range(len(dictonary)):
+            print((dictonary[k]),end=" ")
+            k=k+1
 
 
 
